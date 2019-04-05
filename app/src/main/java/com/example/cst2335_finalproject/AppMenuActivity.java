bad4732202735acb615cb2c84b4e3164ade4200c
Minh Tran <tnminh98@gmail.com>
2019-04-05 05:51:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cst2335_finalproject/AppMenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.cst2335_finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.appmenu_activity);
+    }
+}
